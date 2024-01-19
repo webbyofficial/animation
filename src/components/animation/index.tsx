aa73b7c8f8bd77f3ccaa59267158a9f8299c41ec
nyikonzoli <33204609+nyikonzoli@users.noreply.github.com>
2024-01-19 16:14:35 +0100

    v1.0.5 working

--- src/components/animation/index.tsx
+++ src/components/animation/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import "./animation.css";
 import React from "react";
 
 export type AnimationProps = {
@@ -12,23 +11,46 @@ export type AnimationProps = {
 export function Animation({ direction, delay, children }: AnimationProps) {
   const { ref, inView } = useInView();
   const [counter, setCounter] = useState(0);
-  const classNameHidden = "animation_" + direction + "_hidden";
-  const classNameShow = "animation_" + direction + "_show";
+  const transformBeforaAnimation = {
+    left: "translateX(-100%)",
+    right: "translateX(100%)",
+    top: "translateY(-50px)",
+    bottom: "translateY(100px)",
+  }[direction];
+  const transformAfterAnimation = {
+    left: "translateX(0px)",
+    right: "translateX(0px)",
+    top: "translateY(0px)",
+    bottom: "translateY(0px)",
+  }[direction];
 
   useEffect(() => {
     if (inView) setCounter(1);
   }, [inView]);
 
+  const beforeOrAfterAnimation = (before: any, after: any) => {
+    if (counter < 1) {
+      if (inView) return after;
+      else return before;
+    }
+    return after;
+  };
+
+  console.log(!!delay);
+
   return (
     <div
       ref={ref}
       style={{
-        transitionDelay: (delay ? delay : 0) + "ms",
         backgroundColor: "red",
+        opacity: beforeOrAfterAnimation(0, 1),
+        transition: "all 1s " + (delay ? delay : 0) + "ms",
+        width: "auto",
+        transform: beforeOrAfterAnimation(
+          transformBeforaAnimation,
+          transformAfterAnimation
+        ),
       }}
-      className={`${"animation_base"} ${classNameHidden} ${
-        counter < 1 ? (inView ? " " + classNameShow : "") : " " + classNameShow
-      }`}
     >
       {children}
     </div>
